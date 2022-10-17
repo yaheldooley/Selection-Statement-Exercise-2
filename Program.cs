38baa5f7047db17fedccb2e636c058d05bfac65b
yahel dooley <ydooley@gmail.com>
2022-10-17 15:53:46 -0700

    Completed school subject exercise

--- Program.cs
+++ Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is you favorite school subject?");
+            string userInput = Console.ReadLine();
+            userInput = userInput.ToLower();
+
+
+            bool validInput = false;
+            switch(userInput)
+            { 
+                case "math":
+                validInput = true;
+                break;
+
+                case "science":
+                validInput = true;
+                break;
+
+                case "social studies":
+                validInput = true;
+                break;
+
+                case "history":
+                validInput = true;
+                break;
+
+                case "biology":
+                validInput = true;
+                break;
+            
+            }
+
+            string message = validInput ? $"I think {userInput} is my favorite, too!" : "Yeah, that's not my favorite subject.";
+            Console.WriteLine(message);
+            Console.ReadLine();
         }
     }
 }
